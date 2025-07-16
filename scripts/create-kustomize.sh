19e7225bec9631ae6ae290fce6a62740cfcb1947
somnathbm <somnathbm0@gmail.com>
2025-07-16 13:10:44 +0530

    modified kustomize/helm template generation

--- scripts/create-kustomize.sh
+++ scripts/create-kustomize.sh
@@ -2,33 +2,37 @@
 
 set -e
 
-# Export the raw templates
-helm template my-opd-svc ./charts/opd-service -f ./kustomize/overlays/dev/opd/opd-values.yaml > ./kustomize/base/opd/opd-service.yaml
-helm template my-billing-svc ./charts/billing-service -f ./kustomize/overlays/dev/billing/billing-values.yaml > ./kustomize/base/billing/billing-service.yaml
-helm template my-appointment-svc ./charts/appointment-service -f ./kustomize/overlays/dev/appointment/appointment-values.yaml > ./kustomize/base/appointment/appointment-service.yaml
+# Export the raw templates in `/templates`
+helm template my-opd-svc ./charts/opd-service -f ./kustomize/overlays/dev/opd/opd-values.yaml > ../templates/opd-service.yaml
+helm template my-billing-svc ./charts/billing-service -f ./kustomize/overlays/dev/billing/billing-values.yaml > ../templates/billing-service.yaml
+helm template my-appointment-svc ./charts/appointment-service -f ./kustomize/overlays/dev/appointment/appointment-values.yaml > ../templates/appointment-service.yaml
 
-# generate the Kustomization.yaml file
-cat >./kustomization.yaml << EOF
-    apiVersion: kustomize.config.k8s.io/v1beta1
-    kind: Kustomization
-    namespace: hospital-dev
+# Generate the namespace.yaml file in `/templates`
+cat >../templates/namespace.yaml <<EOF
+  apiVersion: v1
+  kind: Namespace
+  metadata:
+    name: hospital-dev
+EOF
 
-    resources:
+# Generate the Kustomization.yaml file in `/templates`
+cat >../templates/kustomization.yaml << EOF
+  apiVersion: kustomize.config.k8s.io/v1beta1
+  kind: Kustomization
+  namespace: hospital-dev
+
+  resources:
+    - namespace.yaml
     - opd-service.yaml
     - billing-service.yaml
     - appointment-service.yaml
 EOF
 
-# Check existance of kustomization.yaml file
-cat ./kustomization.yaml
-
-if [[ $? != 0 ]]; then
-  echo "Error creating kustomization.yaml"
+# Check existance of `/templates/kustomization.yaml` file
+if [[ -e "../templates/kustomization.yaml" ]]; then
+  echo "kustomization.yaml file does not exist"
   exit 1
 else
-  # create the "hospotal-dev" namespace first
-  kubectl create ns hospital-dev
-
   # apply the kustomization.yaml
-  kubectl apply -k .  
+  kubectl apply -k ../templates
 fi
