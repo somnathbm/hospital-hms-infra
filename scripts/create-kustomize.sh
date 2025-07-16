34dfd1f85e8fe231e7b1677da516489400cdcb1a
somnathbm <somnathbm0@gmail.com>
2025-07-16 14:23:21 +0530

    updated create-kustomize script

--- scripts/create-kustomize.sh
+++ scripts/create-kustomize.sh
@@ -2,13 +2,16 @@
 
 set -e
 
+# Create `/templates` directory
+mkdir -p templates
+
 # Export the raw templates in `/templates`
-helm template my-opd-svc ./charts/opd-service -f ./kustomize/overlays/dev/opd/opd-values.yaml > ../templates/opd-service.yaml
-helm template my-billing-svc ./charts/billing-service -f ./kustomize/overlays/dev/billing/billing-values.yaml > ../templates/billing-service.yaml
-helm template my-appointment-svc ./charts/appointment-service -f ./kustomize/overlays/dev/appointment/appointment-values.yaml > ../templates/appointment-service.yaml
+helm template my-opd-svc ./charts/opd-service -f ./kustomize/overlays/dev/opd/opd-values.yaml > ./templates/opd-service.yaml
+helm template my-billing-svc ./charts/billing-service -f ./kustomize/overlays/dev/billing/billing-values.yaml > ./templates/billing-service.yaml
+helm template my-appointment-svc ./charts/appointment-service -f ./kustomize/overlays/dev/appointment/appointment-values.yaml > ./templates/appointment-service.yaml
 
 # Generate the namespace.yaml file in `/templates`
-cat >../templates/namespace.yaml <<EOF
+cat >./templates/namespace.yaml <<EOF
   apiVersion: v1
   kind: Namespace
   metadata:
@@ -16,7 +19,7 @@ cat >../templates/namespace.yaml <<EOF
 EOF
 
 # Generate the Kustomization.yaml file in `/templates`
-cat >../templates/kustomization.yaml << EOF
+cat >./templates/kustomization.yaml << EOF
   apiVersion: kustomize.config.k8s.io/v1beta1
   kind: Kustomization
   namespace: hospital-dev
@@ -29,10 +32,10 @@ cat >../templates/kustomization.yaml << EOF
 EOF
 
 # Check existance of `/templates/kustomization.yaml` file
-if [[ -e "../templates/kustomization.yaml" ]]; then
-  echo "kustomization.yaml file does not exist"
+if [[ ! -f "../templates/kustomization.yaml" ]]; then
+  echo "kustomization.yaml file does not exist. Cannot continue."
   exit 1
 else
   # apply the kustomization.yaml
-  kubectl apply -k ../templates
+  kubectl apply -k ./templates
 fi
